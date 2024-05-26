9249b4d62e931609a141e55c67220d750835ce69
rd273001 <rd273001@gmail.com>
2024-05-26 07:11:37 +0530

    Minor changes

--- client/src/utils/apiService.js
+++ client/src/utils/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create( {
-  baseURL: `http://localhost:5000/api`,
+  baseURL: `https://certificate-generator-yof0.onrender.com/api`,
 } );
 
 export default api;
